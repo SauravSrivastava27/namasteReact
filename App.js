26760a47ec6439cf41f3a1561657b1cdf55bb109
SauravSrivastava27 <saurav.srivastava27@outlook.com>
2026-06-04 21:41:40 +0530

    react learn

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 const header = React.createElement('div', { id: "parent" }, [
-    React.createElement('div', { id: "child" }, [
+    React.createElement('div', { id: "child", key: "child11" }, [
         React.createElement('h1', {}, "I am h1 tag"),
         React.createElement('h2', {}, "I am h2 tag")
     ]),
-    React.createElement('div', { id: "child2" }, [
+    React.createElement('div', { id: "child2", key: "child12" }, [
         React.createElement('h1', {}, "I am h1 tag"),
         React.createElement('h2', {}, "I am h2 tag")
     ])
